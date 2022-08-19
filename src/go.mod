1633b8052636470dc47bdefed9e340824b24e62e
Viktor7421 <victor.ascuna@utec.edu.pe>
2022-08-18 21:11:16 -0500

    update2

--- src/go.mod
+++ src/go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/rs/cors v1.8.2
 )
--- src/handlers/test.go
+++ src/handlers/test.go
@@ -3,7 +3,21 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/jinzhu/gorm"
 )
+
+type Usuario struct {
+	gorm.Model
+
+	GoogleId string `json:"GoogleId"`
+
+	Nombre string `json:"Nombre"`
+
+	Cargo string `json:"Cargo"`
+
+	Correo string `json:"Correo"`
+}
 
 func GetFunc(w http.ResponseWriter, r *http.Request) {
 
